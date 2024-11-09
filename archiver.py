a3049cb608060ca8c921e09cd8f643beb7f9dd72
silverymist <26202085+silverymist@users.noreply.github.com>
2024-11-09 16:41:00 +0100

    fix folder to dataframe matching

--- archiver.py
+++ archiver.py
@@ -12,7 +12,9 @@ BUILD_DIR = os.path.join(*os.getenv('COMPILE_DIR').split('/'))
 
 
 def copy_source_files(source: str, target: str):
-    for root, _, files in os.walk(source):
+    source_path = os.path.join(SOURCE_DIR, source)
+    counter = 0
+    for root, _, files in os.walk(source_path):
         for filename in files:
             file, ext = os.path.splitext(filename)
             if 'readme' in file.lower() or ext == '.c' or ext == '.h':
@@ -20,28 +22,45 @@ def copy_source_files(source: str, target: str):
                 destination_folder = os.path.join(target, os.path.relpath(root, start=SOURCE_DIR))
                 os.makedirs(destination_folder, exist_ok=True)
                 shutil.copy(filepath, destination_folder)
+                counter += 1
+    print(f"{counter} source files")
 
 
 def copy_build_files(source: str, target: str):
-    for root, _, files in os.walk(source):
+    source_path = os.path.join(BUILD_DIR, source)
+    counter = 0
+    for root, _, files in os.walk(source_path):
         for filename in files:
             filepath = os.path.join(root, filename)
             if os.access(filepath, os.X_OK) or filename.lower().endswith('.exe'):
                 destination_folder = os.path.join(target, os.path.relpath(root, start=BUILD_DIR))
                 os.makedirs(destination_folder, exist_ok=True)
                 shutil.copy(filepath, destination_folder)
+                counter += 1
+    print(f"{counter} build files")
 
 
 def folders_to_zip(source: str, target: str, df: pd.DataFrame):
     archives = [x for x in os.scandir(source) if x.is_dir()]
     for entry in archives:
-        if entry.name not in df.index:
-            print(f"Archive '{entry.name}' not found in DataFrame")
+        row_found = match_folder_to_row(entry.name, df)
+        if row_found is None:
             continue
         zip_path = os.path.join(target, f"{entry.name}.zip")
         shutil.make_archive(zip_path[:-4], 'zip', entry.path)
+        df.at[row_found.index, 'Archived'] = True
 
-        df.at[entry.name, 'Archived'] = True
+
+def match_folder_to_row(folder_name: str, df: pd.DataFrame) -> pd.Series | None:
+    row_copy = df.query(f"Folder == '{folder_name}'").copy()
+    if len(row_copy) == 0:
+        print(f"Folder '{folder_name}' not found in DataFrame")
+        return None
+    if len(row_copy) > 1:
+        print(f"Folder '{folder_name}' appears in DataFrame multiple times")
+        return None
+    # exactly one match found
+    return row_copy.iloc[[0]]
 
 
 if __name__ == "__main__":
@@ -51,9 +70,14 @@ if __name__ == "__main__":
 
     repos = [x for x in os.scandir(BUILD_DIR) if x.is_dir()]
     for entry in repos:
-        if entry.name not in df.index:
-            print(f"Build '{entry.name}' not found in DataFrame")
+        print()
+        row_found = match_folder_to_row(entry.name, df)
+        if row_found is None:
             continue
+        if row_found['Execs'] == '' or pd.isna(row_found['Execs']):
+            print(f"Build '{entry.name}' contains no executables")
+            continue
+        print(f"Archiving {entry.name}...")
         copy_source_files(entry.name, arch_dir)
         copy_build_files(entry.name, arch_dir)
 
